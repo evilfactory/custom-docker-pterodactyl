39f52939ea7501ae03eff0b40d4bb79b808d5faa
EvilFactory <evilfactory@users.noreply.github.com>
2023-02-19 18:55:43 -0300

    Fixed permission

--- entrypoint.sh
+++ entrypoint.sh
@@ -106,6 +106,7 @@ MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
 touch custom_script.sh
+chmod +x custom_script.sh
 
 # Run the Server
 eval ${MODIFIED_STARTUP}
